2a094879c68b56f9d046a03ea656724415256d00
Robert Nguyen <thanhnhatminhnguyen@gmail.com>
2018-08-09 15:31:20 -0700

    Stage 3 Implementation

--- js/script.js
+++ js/script.js
@@ -1,15 +1,35 @@
-var no_of_clicks_1 = 0;
-var no_of_clicks_2 = 0;
+var num_clicks = [0, 0, 0, 0, 0];
 
-$('#1 .name').html($('#1 img').attr("alt"));
-$('#2 .name').html($('#2 img').attr("alt"));
+var img_src = ["img/bigcat.jpg",
+							 "img/cat.jpg",
+							 "img/golden.jpg",
+							 "img/samoyed.jpg",
+							 "img/shiba.jpg"];
 
+var cat_name = ["Mr. Tiger",
+								"Honest Cat",
+								"Beautiful in Golden",
+								"I am a dog not cat",
+								"Who I am Where I am from"]
 
-$('#1 img').click(function(e) {
-	$('#1 .result').html("Number of clicks: " + ++no_of_clicks_1);
-});
+for (var i = 0; i < num_clicks.length; i++) {
+	
+	var elem = document.createElement('li');
+	elem.textContent = cat_name[i];
+	
+	elem.addEventListener('click', ( function(num) {
+		return function() {
+			$('#container .name').html(cat_name[num]);
+			$('#container img').attr("src", img_src[num]);
+			$('#container img').click(function(e){
+				num_clicks[num]++;
+				$('#container .result').html("Number of clicks: " + num_clicks[num]);
+			});
 
+		$('#container .result').html("Number of clicks: " + num_clicks[num]);
 
-$('#2 img').click(function(e) {
-	$('#2 .result').html("Number of clicks: " + ++no_of_clicks_2);
-});
+		}
+	})(i));
+	
+	document.getElementById('list').appendChild(elem);
+}
