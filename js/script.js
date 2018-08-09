2ac856dd235c0627d0ca22fa376ccaa427dc5ba1
Robert Nguyen <thanhnhatminhnguyen@gmail.com>
2018-08-09 14:07:55 -0700

    Stage 2 Implementation

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
-var no_of_clicks = 0;
+var no_of_clicks_1 = 0;
+var no_of_clicks_2 = 0;
 
-$('#cat-pic').click(function(e) {
-	no_of_clicks = no_of_clicks + 1;
-	$('#result').html("Number of clicks: " + no_of_clicks);
+$('#1 .name').html($('#1 img').attr("alt"));
+$('#2 .name').html($('#2 img').attr("alt"));
+
+
+$('#1 img').click(function(e) {
+	$('#1 .result').html("Number of clicks: " + ++no_of_clicks_1);
+});
+
+
+$('#2 img').click(function(e) {
+	$('#2 .result').html("Number of clicks: " + ++no_of_clicks_2);
 });
